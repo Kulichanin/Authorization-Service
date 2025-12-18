bc6698f348b3083366ef915839b875fc3c6e61af
Дмитрий <mr.gn0m@yandex.ru>
2025-12-18 16:59:16 +0300

    add cobra module

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,9 +1,15 @@
-package sso
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/Kulichanin/Authorization-Service/cmd/sso/cmd"
 
 func main() {
 	//  TODO: работа с конфигами через viper
 
-	//  TODO: работа с логами через clog
+	//  TODO: работа с логами через slog
 
 	//  TODO: инициализация приложения через cobra
+	cmd.Execute()
 }
